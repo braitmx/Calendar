d0fb8cd03c0d5cc2aed3bdc1843848c511f40d2a
Alilln <broal@qip.ru>
2017-12-31 19:33:07 +0300

    delete old Add-Task version

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -98,44 +98,6 @@
         </div>
 
         <!--Calendar -->
-
-        <!--Add-task 
-        <form class="add-task" v-show="newTask">
-            <label for="desc">Description:</label>
-            <br />
-            <textarea name="desc" id="desc" cols="30" rows="10" v-model="task.desc"></textarea>
-            <br /><br />
-            <label for="startTime">StartTime:</label>
-            <input type="text" placeholder="example: 02.09 12:40" required v-model="task.startTime">
-            <br />
-            <label for="endTime">EndTime:</label>
-            <input type="text" placeholder="example: 02.09 13:20" required v-model="task.endTime">
-            <br />
-            <label for="period">Periodicity:</label>
-            <br />
-            <select name="period" multiple v-model="task.period">
-                <option value="">-</option>
-                <option value="Mon">Monday</option>
-                <option value="Tue">Tuesday</option>
-                <option value="Wed">Wednesday</option>
-                <option value="Thu">Thursday</option>
-                <option value="Fri">Friday</option>
-                <option value="Sat">Saturday</option>
-                <option value="Sun">Sunday</option>
-            </select>
-            <br /><br />
-            <label for="category">Category:</label>
-            <select name="category" v-model="task.category">
-                <option value="Home">Home</option>
-                <option value="Job">Job</option>
-                <option value="Rest">Rest</option>
-                <option value="Learning">Learning</option>
-            </select>
-            <br /><br />
-            <button @click.prevent="addTask">add</button>
-        </form>
-        Add-task -->
-
     </div>
 </template>
 
@@ -171,33 +133,7 @@
         props: ['uid'],
 
         methods: {
-            /* addTask: function () {
- 
-                 if (this.task.desc && this.task.startTime && this.task.endTime && this.task.period && this.task.category) {
- 
-                     let cloneTask = {}; 
- 
-                     for (let key in this.task) {
-                         cloneTask[key] = this.task[key];
-                     }
- 
-                     this.data.tasks.push(cloneTask);
- 
-                     //first > last
-                 }
- 
-                 Firebase.database().ref('tasks/' + this.uid).set({
-                     data: this.data.tasks
-                 });
- 
- 
-                 for (let key in this.task) {
- 
-                     if (key === 'period') this.task[key] = [];
-                     else this.task[key] = '';
-                 }
-             }, */
-
+           
             dateChange: function () {
 
                 function empty(el) {
